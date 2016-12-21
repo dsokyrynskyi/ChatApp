2150751215f77d73961252b2546e1b6d1518f3de
Star67 <you@example.com>
2016-12-21 23:30:41 +0200

    avatar and new design

--- src/main.go
+++ src/main.go
@@ -50,6 +50,16 @@ func main(){
 	http.Handle("/login", &templateHandler{filename:"login.html"})
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, req *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:"auth",
+			Value:"",
+			Path:"/",
+			MaxAge:-1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 	go r.run()
 	if err := http.ListenAndServe(":8080", nil); err != nil{
 		log.Fatal("ListenAndServe: ", err)
